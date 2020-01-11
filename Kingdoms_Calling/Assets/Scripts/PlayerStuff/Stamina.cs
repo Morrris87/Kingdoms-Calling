eb1c4d52e5eed08834da796436ba067c4893e7e7
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-01-10 19:09:42 -0500

    Update Stamina.cs

--- Kingdoms_Calling/Assets/Scripts/PlayerStuff/Stamina.cs
+++ Kingdoms_Calling/Assets/Scripts/PlayerStuff/Stamina.cs
@@ -10,10 +10,11 @@ using UnityEngine.UI;
 
 public class Stamina : MonoBehaviour
 {
-    public int maxStamina;      // Sets the max value the stamina can go to for the object this script is attached to
-    public Image staminaUI;     // UI element for this object's stamina bar
+    public float maxStamina = 20f;    // Sets the max value the stamina can go to for the object this script is attached to
+    public Image staminaUI;         // UI element for this object's stamina bar
 
-    private int currentStamina; // This int keeps track of the current stamina the object currently has
+    private float currentStamina; // This int keeps track of the current stamina the object currently has
+    private bool isSprinting;   // True when using stamina
     private bool isDepleted;    // If currentStamina reaches 0, this bool is set to true, otherwise false
 
     // Start is called before the first frame update
@@ -26,26 +27,45 @@ public class Stamina : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        // If holding down shift...
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            Deplete(10);
+            Sprinting(true);    // Start sprinting
+        }
+        // If releasing shift...
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Sprinting(false);   // Stop sprinting
+        }
+
+        if (isSprinting)
+        {
+            currentStamina -= 2;   // Decrease stamina over time
+            staminaUI.fillAmount = CalculateStaminaLeftPercent(currentStamina, maxStamina); // Updates the UI
+
+            // If stamina hits 0...
+            if (currentStamina <= 0)
+            {
+                Debug.Log("Stamina Depleted");
+                currentStamina = 0;     // Reset currentStamina to 0
+                isSprinting = false;    // Stop sprinting
+            }
+        }
+        else if (!isSprinting && currentStamina < maxStamina)
+        {
+            currentStamina += 1;
+            staminaUI.fillAmount = CalculateStaminaLeftPercent(currentStamina, maxStamina); // Updates the UI
         }
     }
 
-    private void Deplete(int energy)
+    private void Sprinting(bool sprint)
     {
-        currentStamina -= energy;   // Subtracts energy spent from currentStamina
-        staminaUI.fillAmount = CalculateStaminaLeftPercent(currentStamina, maxStamina);
-
-        if (currentStamina <= 0)
-        {
-
-        }
+        isSprinting = sprint;
     }
 
-    private float CalculateStaminaLeftPercent(int current, int max)
+    private float CalculateStaminaLeftPercent(float current, float max)
     {
-        float energyPercent = (float)current / (float)max;
+        float energyPercent = current / max;
         return energyPercent;
     }
 }
