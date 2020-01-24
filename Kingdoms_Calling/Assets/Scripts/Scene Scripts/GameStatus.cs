9db8a501e29cb4c39549bae5b5b217d587086337
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-01-24 15:26:01 -0500

    Renamed Scenes to work with LoadScene

---
+++ Kingdoms_Calling/Assets/Scripts/Scene Scripts/GameStatus.cs
@@ -0,0 +1,32 @@
+﻿//  Name: GameStatus.cs
+//  Author: Connor Larsen
+//  Date: 1/24/2020
+//  Resource: Unity Tutorial: Preserving Data between Scene Loading/Switching
+//  Link: https://www.youtube.com/watch?v=WchH-JCwVI8
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStatus : MonoBehaviour
+{
+    // Variables to be stored when switching scenes
+    [HideInInspector] public Health p1Health, p2Health, p3Health, p4Health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // When called, loads the scene with the name matching sceneName
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
