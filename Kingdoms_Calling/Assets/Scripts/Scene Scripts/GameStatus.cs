c6fc040ff30ccd0c5b51022f45f7e49a2ce3e202
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-01-24 17:03:38 -0500

    Update GameStatus.cs

--- Kingdoms_Calling/Assets/Scripts/Scene Scripts/GameStatus.cs
+++ Kingdoms_Calling/Assets/Scripts/Scene Scripts/GameStatus.cs
@@ -1,21 +1,30 @@
 ﻿//  Name: GameStatus.cs
 //  Author: Connor Larsen
 //  Date: 1/24/2020
-//  Resource: Unity Tutorial: Preserving Data between Scene Loading/Switching
-//  Link: https://www.youtube.com/watch?v=WchH-JCwVI8
+//  Resource: Unity Tutorial: Preserving Data between Scene Loading/Switching - https://www.youtube.com/watch?v=WchH-JCwVI8
 
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStatus : MonoBehaviour
 {
-    // Variables to be stored when switching scenes
-    private Health p1Health, p2Health, p3Health, p4Health;
+    // Private Variables
+    static private GameStatus instance;  // Keeps track of this instance of GameStatus, in the case of duplicates
+    private Health p1Health, p2Health, p3Health, p4Health;  // Keeps track of each player's health values
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance != null)  // If instance is already in use...
+        {
+            // Destroy this instance of GameStatus
+            Destroy(this.gameObject);
+        }
+        else    // instance is not in use, meaning we can store this instance
+        {
+            instance = this;                           // Stores this instance of GameStatus as our current and main instance
+            GameObject.DontDestroyOnLoad(instance);    // When a new scene is loaded, GameStatus is carried over, keeping hold of variables we need
+        }
     }
 
     // Update is called once per frame
