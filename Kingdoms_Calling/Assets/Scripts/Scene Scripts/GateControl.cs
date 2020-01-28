b276d538ed69d5504fb3c9e645ddbc4e847f7267
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-01-27 20:01:09 -0500

    Update GateControl.cs

--- Kingdoms_Calling/Assets/Scripts/Scene Scripts/GateControl.cs
+++ Kingdoms_Calling/Assets/Scripts/Scene Scripts/GateControl.cs
@@ -7,16 +7,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GateControl : MonoBehaviour
-{  
+{
     // Private Variables
+    private GameStatus gameStatus;
     private Collider gateCollider;  // Variable to hold the collider for the gate
-    private bool nextLevelAccess;   // Bool to control if the collider is enabled or not
+    [SerializeField] private bool nextLevelAccess;   // Bool to control if the collider is enabled or not
 
     // Start is called before the first frame update
     void Start()
     {
         gateCollider = this.GetComponent<Collider>();   // Stores the collider on the gate into gateCollider
         nextLevelAccess = false;
+        gameStatus = FindObjectOfType<GameStatus>();
     }
 
     // Update is called once per frame
@@ -24,11 +26,11 @@ public class GateControl : MonoBehaviour
     {
         if (nextLevelAccess)
         {
-            gateCollider.enabled = false;
+            gateCollider.isTrigger = true;
         }
         else
         {
-            gateCollider.enabled = true;
+            gateCollider.isTrigger = false;
         }
     }
 
@@ -42,5 +44,14 @@ public class GateControl : MonoBehaviour
     public void SetNextLevelAccess(bool value)
     {
         nextLevelAccess = value;
+    }
+
+    // Called whenever a Collider enters the gate's box collider
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && nextLevelAccess)
+        {
+            gameStatus.LoadScene("BradleyTest");
+        }
     }
 }
