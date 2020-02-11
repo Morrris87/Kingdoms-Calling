4a0eacf3e3c20475187b361b638564ae2f70ddce
zaclink97 <51802916+zaclink97@users.noreply.github.com>
2020-02-11 17:27:09 -0500

    map things

---
+++ Kingdoms_Calling/Assets/MapPopUp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapPopUp : MonoBehaviour
+{
+    public Image map;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnTriggerEnter(player.GetComponent<MeshCollider>());
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        // pop up a thing that says "HIT A TO READ MAP"
+        // when A is hit pause game Display Image
+        // to close HIT A AGAIN
+        // unpause
+    }
+}
