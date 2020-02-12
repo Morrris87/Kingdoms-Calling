ed4259c8cdc13b962f0b618dbd85ba1bf1598c32
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-02-11 19:50:14 -0500

    Axe Whirlwind Functionality

--- Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/AxeWhirlwind.cs
+++ Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/AxeWhirlwind.cs
@@ -1,157 +1,124 @@
 ﻿/*
- * Warrior axe whirlwind ability
+ * Warrior Axe Whirlwind Ability
  * Resource: https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/Installation.html
- * Created by: Bradley Williamson
+ * Created by: Bradley Williamson, Connor Larsen
  * On: 1/15/20
  */
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.UI;
+
 public class AxeWhirlwind : MonoBehaviour
 {
-    [Header("UI Element")]
-    public Image abilityUI; // The UI Images for the abilities
+    // Public Variables
+    public GameObject abilityCooldownUI;    // UI Element for the ability cooldown in the HUD
+    public GameObject areaOfEffect;         // The collider for the Taunt ability
+    public float waitTime = 40f;            // Time in seconds needed to wait for ability cooldown
+    [HideInInspector] public bool isUsable; // When ability is available for use, set this to true
 
-    [Header("Ability Specs")]
-    public float whirlwindCooldown;     //Cooldown time for the ability itself
-    public float whirlwindRadius;       //How big the AOE is
-    public float whirlwindDuration;     //How long the skill lasts
-    public int whirlwindDamage;       //How much damage the skill does at each tick
-    public float whirlwindAttackSpeed;  //How often damage ticks on enemies in the area
-    public float comboCooldown;
-
-    [Header("ComboAbility Prefabs")]
-    public GameObject ArcherWarriorComboPrefab;
-
-    [HideInInspector]
-    public bool isUsable;          // When ability is available for use, set this to true
-    private bool inUse;          // When ability is in use, set this to false
-    private float cooldownElapsed;  // When in cooldown, increments until waitTime is reached
-    private int playerLayerIndex, enemyLayerIndex;      //Player and enemy layer index
-    private float attackDuration, attackInterval;
-    private ElementManager eManager;
-    private AssassinWarriorCombo assassinWarriorCombo;
-
+    // Private Variables
+    private float cooldownTimer;    // When in cooldown, increments until waitTime is reached
 
     // Start is called before the first frame update
     void Start()
     {
-        isUsable = true;        // Ability starts as usable
-        inUse = true;        // Ability starts as not in use
-        cooldownElapsed = 0;    // Cooldown timer starts at 0 (Not on cooldown)
-        attackDuration = whirlwindDuration; // Set the duration to the inpsector value
-        attackInterval = whirlwindAttackSpeed; // Set the interval for attacking
-
-        //Get the player and enemy layermask id's
-        playerLayerIndex = LayerMask.NameToLayer("Player");
-        enemyLayerIndex = LayerMask.NameToLayer("Enemy");
+        isUsable = true;            // Ability starts as usable
+        cooldownTimer = waitTime;   // Cooldown timer starts at the value of waitTime
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if unusable decrease the timer
+        // If ability has been used and hasn't refreshed...
         if (isUsable == false)
         {
-            cooldownElapsed -= Time.deltaTime;                  // Count the cooldown down
-            if (cooldownElapsed <= 0)
+            // If cooldownTimer hasn't completed...
+            if (cooldownTimer >= 0f)
             {
-                isUsable = true;                                // Reset the isUsable field
+                // Subtract cooldownTimer by deltaTime
+                cooldownTimer -= Time.deltaTime;
+
+                // Update the UI with the amount of time remaining
+                abilityCooldownUI.GetComponentInChildren<Text>().text = "" + ((int)cooldownTimer + 1);
             }
-            if (inUse)                                           // If we are currently using the ability
+            // Otherwise cooldownTimer has completed
+            else
             {
-                if (attackDuration >= 0)
-                    attackDuration -= Time.deltaTime;               // Count the duration left down
-                else
-                {
-                    inUse = false;                                  // Disable the ability
-                }
-                if (attackInterval >= 0)
-                    attackInterval -= Time.deltaTime;               // Count down attack interval
-                else if (attackInterval <= 0)
-                {
-                    AttackAroundCharacter();                        //If timer is less than or 0 we attack around the character
-                }
+                isUsable = true;                    // Make ability useable again
+                abilityCooldownUI.SetActive(false); // Hide the cooldown UI
+                cooldownTimer = waitTime;           // Reset the cooldownTimer
             }
-        }
-        //if our combo is on cooldown decrease the timer
-        if(comboCooldown >=0)
-        {
-            comboCooldown -= Time.deltaTime;
         }
     }
 
     // Calling this function uses the ability
     public void UseAbility()
     {
-        Debug.Log("Whirlwind Used (Warroir)");
-
+        // If the ability is usable...
         if (isUsable == true)
         {
-            // Ability has been used, so it needs to cooldown
+            // Ability has been used, so set ability as unusable
             isUsable = false;
 
-            // Start the cooldown timer
-            cooldownElapsed = whirlwindCooldown;
-            // Update the UI with the time remaining
+            // Enable the cooldown UI
+            abilityCooldownUI.SetActive(true);
 
-            // Play the ability animation            
+            // Play the ability animation
 
-            // Change the inUse bool
-            inUse = true;
+            // Place the collder for the ability in the spawn area
+            Instantiate(areaOfEffect, transform.position, Quaternion.identity);
         }
     }
 
-    void AttackAroundCharacter()
-    {
-        //Debug.Log("Attacking Started");
-        //Grab all colliders inside of the sphere which in our case acts as a circle with enemy layer mask 
-        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, whirlwindRadius, 1 << enemyLayerIndex);
+    //void AttackAroundCharacter()
+    //{
+    //    //Debug.Log("Attacking Started");
+    //    //Grab all colliders inside of the sphere which in our case acts as a circle with enemy layer mask 
+    //    Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, whirlwindRadius, 1 << enemyLayerIndex);
 
-        //Loop through the colliders checking if its either a player or a enemy
-        int i = 0;
-        while (i < hitColliders.Length)
-        {
-            eManager = hitColliders[i].GetComponent<ElementManager>();
-            //if our combo is off cooldown then check elements
-            if (comboCooldown <= 0)
-            {
-                // Check enemy procs for combos
-                if (eManager.thisElement == ElementManager.ClassElement.Wind)  // If enemy has a wind proc...
-                {
-                    // Activate the Archer combo
-                    //instantiate the archer warrior combo prefab on target location
-                    Instantiate(ArcherWarriorComboPrefab, hitColliders[i].transform.position, Quaternion.identity);
-                }
-                else if (eManager.thisElement == ElementManager.ClassElement.Earth)    // If enemy has a earth proc...
-                {
-                    // Activate the Paladin combo
-                }
-                else if (eManager.thisElement == ElementManager.ClassElement.Lightning) // If enemy has a lightning proc...
-                {
-                    // Activate the Assassin combo
-                    assassinWarriorCombo.UseAbility();
-                }
-            }
-            //else our combo is on cooldown just do the normal damage
-            else
-            {
-                // Enemy has no proc and ability happens as normal
-                //Debug.Log("Whirlwind Damage " + hitColliders[i].name);
-                hitColliders[i].gameObject.GetComponentInChildren<Health>().Damage(whirlwindDamage); //Damage the current colliders health by the current damageValue
+    //    //Loop through the colliders checking if its either a player or a enemy
+    //    int i = 0;
+    //    while (i < hitColliders.Length)
+    //    {
+    //        eManager = hitColliders[i].GetComponent<ElementManager>();
+    //        //if our combo is off cooldown then check elements
+    //        if (comboCooldown <= 0)
+    //        {
+    //            // Check enemy procs for combos
+    //            if (eManager.thisElement == ElementManager.ClassElement.Wind)  // If enemy has a wind proc...
+    //            {
+    //                // Activate the Archer combo
+    //                //instantiate the archer warrior combo prefab on target location
+    //                Instantiate(ArcherWarriorComboPrefab, hitColliders[i].transform.position, Quaternion.identity);
+    //            }
+    //            else if (eManager.thisElement == ElementManager.ClassElement.Earth)    // If enemy has a earth proc...
+    //            {
+    //                // Activate the Paladin combo
+    //            }
+    //            else if (eManager.thisElement == ElementManager.ClassElement.Lightning) // If enemy has a lightning proc...
+    //            {
+    //                // Activate the Assassin combo
+    //                assassinWarriorCombo.UseAbility();
+    //            }
+    //        }
+    //        //else our combo is on cooldown just do the normal damage
+    //        else
+    //        {
+    //            // Enemy has no proc and ability happens as normal
+    //            //Debug.Log("Whirlwind Damage " + hitColliders[i].name);
+    //            hitColliders[i].gameObject.GetComponentInChildren<Health>().Damage(whirlwindDamage); //Damage the current colliders health by the current damageValue
 
-                // Give enemies procs if appliciable
-                if (eManager.thisElement == ElementManager.ClassElement.NONE)
-                {
-                    eManager.thisElement = ElementManager.ClassElement.Fire;
-                }
-            }
+    //            // Give enemies procs if appliciable
+    //            if (eManager.thisElement == ElementManager.ClassElement.NONE)
+    //            {
+    //                eManager.thisElement = ElementManager.ClassElement.Fire;
+    //            }
+    //        }
 
-            i++;
-        }
-        //Reset the tick timer 
-        attackInterval = whirlwindAttackSpeed;
-        //Debug.Log("Attacking Ended");
-    }
+    //        i++;
+    //    }
+    //    //Reset the tick timer 
+    //    attackInterval = whirlwindAttackSpeed;
+    //    //Debug.Log("Attacking Ended");
+    //}
 }
---
+++ Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/AxeWhirlwindCollider.cs
@@ -0,0 +1,86 @@
+﻿//  Name: AxeWhirlwindCollider.cs
+//  Author: Connor Larsen
+//  Function: Places a collider at the point the player is standing which damages enemies inside over time
+
+using UnityEngine;
+
+public class AxeWhirlwindCollider : MonoBehaviour
+{
+    // Public Variables
+    public float timerLength = 5f;  // Time in seconds for the collider to last before being destroyed
+    public GameObject playerPos;    // Keeps track of the player's position
+
+    // Private Variables
+    private float damageInterval = 0.5f;    // Interval value for how much time passes between damage being dealt
+    private float damageTimer;              // Timer for timing damage intervals
+    private float abilityLifeTimer;         // The ability timer
+    private float warriorDmg;               // Variable for the archer's attack damage
+    private bool cooldownActive;            // Bool which determines if the cooldown is running
+
+    // Combo Variables
+    private ArcherWarriorCombo archerWarriorCombo;      // Used for calling the archer combo
+    private AssassinWarriorCombo assassinWarriorCombo;  // Used for calling the assassin combo
+    private PaladinWarriorCombo paladinWarriorCombo;    // Used for calling the paladin combo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abilityLifeTimer = timerLength; // Sets the length of the cooldown to the amount stored in timerLength
+        cooldownActive = true;          // Starts the cooldown timer
+        damageTimer = 0f;               // Set the damage interval timer
+
+        // Grab the player
+        playerPos = GameObject.FindGameObjectWithTag("Player");
+
+        // Sets archerDmg to the stored value in BasicAttack and halves it
+        warriorDmg = (FindObjectOfType<BasicAttack>().CharacterAttackValue(BasicAttack.CharacterClass.Warrior) / 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the abilityLifeTimer has time left and cooldownActive is true...
+        if (abilityLifeTimer > 0f && cooldownActive)
+        {
+            // Decrease timer by deltaTime every frame
+            abilityLifeTimer -= Time.deltaTime;
+
+            // Move collider to the player
+            transform.position = playerPos.transform.position;
+
+            // If the damage cooldown has completed...
+            if (damageTimer <= 0f)
+            {
+                DamageEnemiesInCollider();      // Call the damage function
+                damageTimer = damageInterval;   // Reset the damage interval timer
+            }
+            else
+            {
+                damageTimer -= Time.deltaTime;  // Decrease timer by deltaTime every frame
+            }
+        }
+
+        // If the cooldown has finished...
+        if (abilityLifeTimer <= 0f)
+        {
+            // Stops the cooldown timer
+            cooldownActive = false;
+
+            // Destroys the collider for the ability
+            Destroy(gameObject);
+        }
+    }
+
+    public void DamageEnemiesInCollider()
+    {
+        // Grab all colliders in the hitbox of the ability
+        Collider[] cols = Physics.OverlapBox(GetComponent<Collider>().bounds.center, GetComponent<Collider>().bounds.extents, GetComponent<Collider>().transform.rotation, LayerMask.GetMask("Enemy"));
+
+        // Cycle through each collider in the cols array
+        foreach (Collider c in cols)
+        {
+            // Deal damage to the enemy
+            c.GetComponent<Health>().Damage((int)warriorDmg);
+        }
+    }
+}
