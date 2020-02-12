1ef4e54ed892b506e00d356f44b7b0e2de46f4c4
brutalgoose23 <59624693+brutalgoose23@users.noreply.github.com>
2020-02-11 19:50:31 -0500

    Flaming Leap Functionality

--- Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/FlamingLeap.cs
+++ Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/FlamingLeap.cs
@@ -1,111 +1,116 @@
 ﻿/*
- * Warrior flaming leap ability
+ * Warrior Flaming Leap Ability
  * Resource: https://docs.unity3d.com/Packages/com.unity.inputsystem@1.0/manual/Installation.html
- * Created by: Bradley Williamson
+ * Created by: Bradley Williamson, Connor Larsen
  * On: 1/16/20
  */
 
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FlamingLeap : MonoBehaviour
 {
+    // Public Variables
     [Header("UI Element")]
-    public Image abilityUI; // The UI Images for the abilities
+    public GameObject abilityCooldownUI;    // UI element for the ability cooldown in the HUD
+    public GameObject areaOfEffect;         // The collider for the FlamingLeap ability
+    public Transform playerDestPos;         // The destination position the player will leap to when the ability is used
+    public float waitTime = 20f;            // Time in seconds needed to wait for ability cooldown
+    [HideInInspector] public bool isUsable; // When ability is available for use, set this to true
 
-    [Header("Ability Specs")]
-    public float leapCooldown;     //Cooldown time for the ability itself
-    public float leapRadius;       //How big the AOE is
-    public float leapDuration;     //How long the skill lasts
-    public int leapDamage;       //How much damage the skill does at each tick
-    public float animationLength;
-    public float leapDistance;
-    public GameObject destinationMarker;
-    Vector2 leapLocation;   
+    // Private Variables
+    private float cooldownTimer;    // When in cooldown, increments until waitTime is reached
+    private Transform tempDestPos;  // Stores the position of the destination before the player moves
 
-    [HideInInspector]
-    public bool isUsable;          // When ability is available for use, set this to true
-    private float cooldownElapsed;  // When in cooldown, increments until waitTime is reached
-    private int playerLayerIndex, enemyLayerIndex;      //Player and enemy layer index
-    float attackDuration, attackInterval;
+    // Old Variables (Might Need?)
+    //public float leapDuration;     //How long the skill lasts
+    //public int leapDamage;       //How much damage the skill does at each tick
+    //public float animationLength;
 
     // Start is called before the first frame update
     void Start()
     {
-        isUsable = true;        // Ability starts as usable
-        cooldownElapsed = 0;    // Cooldown timer starts at 0 (Not on cooldown)
-
-        //Get the player and enemy layermask id's
-        playerLayerIndex = LayerMask.NameToLayer("Player");
-        enemyLayerIndex = LayerMask.NameToLayer("Enemy");
+        isUsable = true;                // Ability starts as usable
+        cooldownTimer = waitTime;       // Cooldown timer starts at the value of waitTime
+        tempDestPos = playerDestPos;    // Initialize the tempDestPos variable
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if unusable decrease the timer
+        // If ability has been used and hasn't refreshed...
         if (isUsable == false)
         {
-            cooldownElapsed -= Time.deltaTime;                  // Count the cooldown down
-            
-            if (cooldownElapsed <= 0)
+            // If cooldownTimer hasn't completed...
+            if (cooldownTimer >= 0f)
             {
-                isUsable = true;                                // Reset the isUsable field
-            } 
+                // Subtract cooldownTimer by deltaTime
+                cooldownTimer -= Time.deltaTime;
+
+                // Update the UI with the amount of time remaining
+                abilityCooldownUI.GetComponentInChildren<Text>().text = "" + ((int)cooldownTimer + 1);
+            }
+            // Otherwise cooldownTimer has completed
+            else
+            {
+                isUsable = true;                    // Make ability useable again
+                abilityCooldownUI.SetActive(false); // Hide the cooldown UI
+                cooldownTimer = waitTime;           // Reset the cooldownTimer
+            }
         }
     }
 
     // Calling this function uses the ability
     public void UseAbility(Vector2 input)
     {
-        //Debug.Log("flame leap Used (Warroir)");
-
+        // If the ability is usable...
         if (isUsable == true)
         {
-            // Ability has been used, so it needs to cooldown
+            // Ability has been used, so set ability as unusable
             isUsable = false;
 
-            // Start the cooldown timer
-            cooldownElapsed = leapCooldown;
-
-            // Update the UI with the time remaining
+            // Enable the cooldown UI
+            abilityCooldownUI.SetActive(true);
 
             // Play the ability animation (handle player location)
 
-            // get the input 
-            leapLocation = input;
-            // Debug.Log(input);
+            // Save the players destPos
+            tempDestPos.position = playerDestPos.position;
 
-            // preform the ability
-            leapCharacter(input);
-            AttackAroundCharacter();
-            GetComponent<CharacterManager>().abilityIndicator.SetActive(false);
-            GetComponent<CharacterManager>().displayLocation = false;
+            // Transfer the player to the destination position
+            transform.position = playerDestPos.position;
+
+            // Place the collder for the ability where the player lands
+            Instantiate(areaOfEffect, transform.position, Quaternion.identity);
+
+            //// preform the ability
+            //leapCharacter(input);
+            //AttackAroundCharacter();
+            //GetComponent<CharacterManager>().abilityIndicator.SetActive(false);
+            //GetComponent<CharacterManager>().displayLocation = false;
         }
     }
 
-    void leapCharacter(Vector2 inp)
-    {
-        //Handle Moving the character
-        //transform.position = (new Vector3(transform.position.x, transform.position.y, transform.position.z) + new Vector3(inp.x * leapDistance,0, inp.y * leapDistance));
-        //transform.position = (new Vector3(transform.position.x, 0, transform.position.z) + new Vector3(inp.x * leapDistance, transform.position.y, inp.y * leapDistance));
-    }
+    //void leapCharacter(Vector2 inp)
+    //{
+    //    //Handle Moving the character
+    //    //transform.position = (new Vector3(transform.position.x, transform.position.y, transform.position.z) + new Vector3(inp.x * leapDistance,0, inp.y * leapDistance));
+    //    //transform.position = (new Vector3(transform.position.x, 0, transform.position.z) + new Vector3(inp.x * leapDistance, transform.position.y, inp.y * leapDistance));
+    //}
 
-    void AttackAroundCharacter()
-    {
-        //Debug.Log("Attacking Started");
-        //Grab all colliders inside of the sphere which in our case acts as a circle with enemy layer mask 
-        Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, leapRadius, 1 << enemyLayerIndex);
+    //void AttackAroundCharacter()
+    //{
+    //    //Debug.Log("Attacking Started");
+    //    //Grab all colliders inside of the sphere which in our case acts as a circle with enemy layer mask 
+    //    Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, leapRadius, 1 << enemyLayerIndex);
 
-        int i = 0;
-        while (i < hitColliders.Length)
-        {
-            //Debug.Log("Warroir Leap Damage " + hitColliders[i].name);
-            hitColliders[i].gameObject.GetComponentInChildren<Health>().Damage(leapDamage); //Damage the current colliders health by the current damageValue
+    //    int i = 0;
+    //    while (i < hitColliders.Length)
+    //    {
+    //        //Debug.Log("Warroir Leap Damage " + hitColliders[i].name);
+    //        hitColliders[i].gameObject.GetComponentInChildren<Health>().Damage(leapDamage); //Damage the current colliders health by the current damageValue
 
-            i++;
-        }
-    }
+    //        i++;
+    //    }
+    //}
 }
---
+++ Kingdoms_Calling/Assets/Scripts/PlayerStuff/Abilities/Warrior/FlamingLeapCollider.cs
@@ -0,0 +1,96 @@
+﻿//  Name: ArrowVolleyCollider.cs
+//  Author: Connor Larsen
+//  Function: 
+
+using UnityEngine;
+
+public class FlamingLeapCollider : MonoBehaviour
+{
+    // Public Variables
+    public float timerLength = 1f;  // Time in seconds for the collider to last before being destroyed
+
+    // Private Variables
+    private float abilityLifeTimer;     // The ability timer
+    private float warriorDmg;           // Variable for the warrior's attack damage
+    private bool cooldownActive;        // Bool which determines if the cooldown is running
+
+    // Combo variables
+    private ArcherWarriorCombo archerWarriorCombo;      // Used for calling the archer combo
+    private AssassinWarriorCombo assassinWarriorCombo;  // Used for calling the assassin combo
+    private PaladinWarriorCombo paladinWarriorCombo;    // Used for calling the paladin combo
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        abilityLifeTimer = timerLength; // Sets the length of the cooldown to the amount stored in timerLength
+        cooldownActive = true;          // Starts the cooldown timer
+
+        // Sets warriorDmg to the stored value in BasicAttack
+        warriorDmg = FindObjectOfType<BasicAttack>().CharacterAttackValue(BasicAttack.CharacterClass.Warrior);
+
+        // Damage all enemies in the collider when placed
+        DamageEnemiesInCollider();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the abilityLifeTimer has time left and cooldownActive is true...
+        if (abilityLifeTimer > 0f && cooldownActive)
+        {
+            // Decrease timer by deltaTime every frame
+            abilityLifeTimer -= Time.deltaTime;
+        }
+
+        // If the cooldown has finished...
+        if (abilityLifeTimer <= 0f)
+        {
+            // Stops the cooldown timer
+            cooldownActive = false;
+
+            // Destroys the collider for the ability
+            Destroy(gameObject);
+        }
+    }
+
+    public void DamageEnemiesInCollider()
+    {
+        // Grab all colliders in the hitbox of the ability
+        Collider[] cols = Physics.OverlapBox(GetComponent<Collider>().bounds.center, GetComponent<Collider>().bounds.extents, GetComponent<Collider>().transform.rotation, LayerMask.GetMask("Enemy"));
+
+        // Cycle through each collider in the cols array
+        foreach (Collider c in cols)
+        {
+            // Deal damage to the enemy
+            c.GetComponent<Health>().Damage((int)warriorDmg);
+
+            // If the enemy currently has no element assigned in it's Element Manager...
+            if (c.GetComponent<ElementManager>().thisElement == ElementManager.ClassElement.NONE)
+            {
+                // Set the elemental proc to Fire
+                c.GetComponent<ElementManager>().thisElement = ElementManager.ClassElement.Fire;
+            }
+            else
+            {
+                // If the enemy currently has an Earth proc...
+                if (c.GetComponent<ElementManager>().thisElement == ElementManager.ClassElement.Earth)
+                {
+                    // Activate the Paladin & Warrior combo
+                    //paladinWarriorCombo.ActivateCombo();
+                }
+                // If the enemy currently has a Wind proc...
+                else if (c.GetComponent<ElementManager>().thisElement == ElementManager.ClassElement.Wind)
+                {
+                    // Activate the Archer & Warrior combo
+                    //archerWarriorCombo.ActivateCombo();
+                }
+                // If the enemy currently has a Lightning proc...
+                else if (c.GetComponent<ElementManager>().thisElement == ElementManager.ClassElement.Lightning)
+                {
+                    // Activate the Archer & Assassin combo
+                    //assassinWarriorCombo.ActivateCombo();
+                }
+            }
+        }
+    }
+}
