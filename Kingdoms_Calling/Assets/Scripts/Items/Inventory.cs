18b1ca12c49355a445b57b77e0e89e2abf1af405
zaclink97 <51802916+zaclink97@users.noreply.github.com>
2020-03-03 09:42:53 -0500

    start of items

---
+++ Kingdoms_Calling/Assets/Scripts/Items/Inventory.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    GameObject characterClass;
+    CharacterManager cManager;
+
+    GameObject itemOneUI;
+    GameObject itemTwoUI;
+    GameObject itemThreeUI;
+    GameObject itemFourUI;
+
+    public Sprite backGround;
+    [HideInInspector]
+    public List<GameObject> ItemList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cManager = GetComponent<CharacterManager>();
+
+        if (cManager.characterClass != CharacterManager.CharacterClass.NONE)
+        {
+            if (cManager.characterClass == CharacterManager.CharacterClass.Archer)
+            {
+                itemOneUI = GameObject.Find("Canvas/PlayerHUD/Archer/Archer_Item1/ItemImage");
+                itemTwoUI = GameObject.Find("Canvas/PlayerHUD/Archer/Archer_Item2/ItemImage");
+                itemThreeUI = GameObject.Find("Canvas/PlayerHUD/Archer/Archer_Item3/ItemImage");
+                itemFourUI = GameObject.Find("Canvas/PlayerHUD/Archer/Archer_Item4/ItemImage");
+            }
+            else if (cManager.characterClass == CharacterManager.CharacterClass.Assassin)
+            {
+                itemOneUI = GameObject.Find("Canvas/PlayerHUD/Assassin/Assassin_Item1/ItemImage");
+                itemTwoUI = GameObject.Find("Canvas/PlayerHUD/Assassin/Assassin_Item2/ItemImage");
+                itemThreeUI = GameObject.Find("Canvas/PlayerHUD/Assassin/Assassin_Item3/ItemImage");
+                itemFourUI = GameObject.Find("Canvas/PlayerHUD/Assassin/Assassin_Item4/ItemImage");
+            }
+            else if (cManager.characterClass == CharacterManager.CharacterClass.Paladin)
+            {
+                itemOneUI = GameObject.Find("Canvas/PlayerHUD/Paladin/Paladin_Item1/ItemImage");
+                itemTwoUI = GameObject.Find("Canvas/PlayerHUD/Paladin/Paladin_Item2/ItemImage");
+                itemThreeUI = GameObject.Find("Canvas/PlayerHUD/Paladin/Paladin_Item3/ItemImage");
+                itemFourUI = GameObject.Find("Canvas/PlayerHUD/Paladin/Paladin_Item4/ItemImage");
+            }
+            else if (cManager.characterClass == CharacterManager.CharacterClass.Warrior)
+            {
+                itemOneUI = GameObject.Find("Canvas/PlayerHUD/Warrior/Warrior_Item1/ItemImage");
+                itemTwoUI = GameObject.Find("Canvas/PlayerHUD/Warrior/Warrior_Item2/ItemImage");
+                itemThreeUI = GameObject.Find("Canvas/PlayerHUD/Warrior/Warrior_Item3/ItemImage");
+                itemFourUI = GameObject.Find("Canvas/PlayerHUD/Warrior/Warrior_Item4/ItemImage");
+            }
+        }
+
+        itemOneUI.GetComponent<Image>().sprite = backGround;
+        itemTwoUI.GetComponent<Image>().sprite = backGround;
+        itemThreeUI.GetComponent<Image>().sprite = backGround;
+        itemFourUI.GetComponent<Image>().sprite = backGround;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Kingdoms_Calling/Assets/Scripts/Items/TombOfStat.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TombOfStat : MonoBehaviour
+{
+    Sprite ItemImage;
+
+    string thisTag;
+
+    enum statType {Health,Power,Speed,Stamina,PhysicalDefence,MagicDefence};
+    statType stat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //get tag
+        thisTag = this.tag;
+        //set stat type to tag
+        if(thisTag == "Health")
+        {
+            stat = statType.Health;
+        }
+        else if (thisTag == "Power")
+        {
+            stat = statType.Power;
+        }
+        else if (thisTag == "Speed")
+        {
+            stat = statType.Speed;
+        }
+        else if (thisTag == "Stamina")
+        {
+            stat = statType.Stamina;
+        }
+        else if (thisTag == "PhysicalDefence")
+        {
+            stat = statType.PhysicalDefence;
+        }
+        else if (thisTag == "MagicDefence")
+        {
+            stat = statType.MagicDefence;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
