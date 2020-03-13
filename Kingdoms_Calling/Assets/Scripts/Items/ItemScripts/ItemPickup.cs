566757f9246d7c506aa3c41d19b9ab303aec3c37
BradleyWilliamson <48719585+BradleyWilliamson@users.noreply.github.com>
2020-03-12 23:16:20 -0400

    Item pickup/drop start

---
+++ Kingdoms_Calling/Assets/Scripts/Items/ItemScripts/ItemPickup.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Item Pickup script placed on Item prefab
+ * Created by: Bradley Williamson
+ * On: 03/12/20
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    [Header("Item to Drop")]
+    public Item.ItemType item;
+
+    Collider thisCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// When we enter a trigger collider check if its a player if yes then add it to their inventory
+    /// and delete the item gameobject
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<Inventory>().AddItem(new Item { itemType = item });
+            Destroy(this.gameObject);
+        }
+    }
+}
