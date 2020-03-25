478dc1480682492e95019ee65f4821dc1ef297b2
zaclink97 <51802916+zaclink97@users.noreply.github.com>
2020-03-25 15:36:58 -0400

    i need an bandaid

--- Kingdoms_Calling/Assets/Scripts/BossShit/BossAiFightOne/BossPatrolState.cs
+++ Kingdoms_Calling/Assets/Scripts/BossShit/BossAiFightOne/BossPatrolState.cs
@@ -34,42 +34,49 @@ public class BossPatrolState : BossFightOneFSMState
     public override void Act()
     {
         int randOne = Random.Range(0, enemyAI.PatrolList.Count);
-        int randTwo = Random.Range(0, enemyAI.PatrolList.Count);
-        int randThree = Random.Range(0, enemyAI.PatrolList.Count);
-        int randFour = Random.Range(0, enemyAI.PatrolList.Count);
+        //int randTwo = Random.Range(0, enemyAI.PatrolList.Count);
+        //int randThree = Random.Range(0, enemyAI.PatrolList.Count);
+        //int randFour = Random.Range(0, enemyAI.PatrolList.Count);
         Debug.Log("Patrolling");
         // Play move animation   
         //move
         speed = enemyAI.bossStats.speed * Time.deltaTime;
         if (donePatrol == false)
         {
-            for (int timesMoved = 0; timesMoved < 4; timesMoved++)// this breaks and wont move at 5 but moves and wont leave at 4
+            //for (int timesMoved = 0; timesMoved < 4; timesMoved++)// this breaks and wont move at 5 but moves and wont leave at 4
+            // {
+            //if (timesMoved == 0)
+            //{
+            //move to randOne
+            if (enemyAI.transform.position != enemyAI.PatrolList[randOne].transform.position)
             {
-                if (timesMoved == 0)
-                {
-                    //move to randOne
-                    enemyAI.transform.LookAt(enemyAI.PatrolList[randOne].transform);
-                    enemyAI.transform.position += enemyAI.transform.forward * speed;
-                }
-                else if (timesMoved == 1)
-                {
-                    //move to randTwo
-                    enemyAI.transform.LookAt(enemyAI.PatrolList[randTwo].transform);
-                    enemyAI.transform.position += enemyAI.transform.forward * speed;
-                }
-                else if (timesMoved == 2)
-                {
-                    //move to randThree
-                    enemyAI.transform.LookAt(enemyAI.PatrolList[randThree].transform);
-                    enemyAI.transform.position += enemyAI.transform.forward * speed;
-                }
-                else if (timesMoved == 3)
-                {
-                    //move to randFour
-                    enemyAI.transform.LookAt(enemyAI.PatrolList[randFour].transform);
-                    enemyAI.transform.position += enemyAI.transform.forward * speed;
-                } 
+                enemyAI.transform.LookAt(enemyAI.PatrolList[randOne].transform);
+                enemyAI.transform.position += enemyAI.transform.forward * speed;
             }
+            else
+            {
+                donePatrol = true;
+            }
+                //}
+                //else if (timesMoved == 1)
+                //{
+                //    //move to randTwo
+                //    enemyAI.transform.LookAt(enemyAI.PatrolList[randTwo].transform);
+                //    enemyAI.transform.position += enemyAI.transform.forward * speed;
+                //}
+                //else if (timesMoved == 2)
+                //{
+                //    //move to randThree
+                //    enemyAI.transform.LookAt(enemyAI.PatrolList[randThree].transform);
+                //    enemyAI.transform.position += enemyAI.transform.forward * speed;
+                //}
+                //else if (timesMoved == 3)
+                //{
+                //    //move to randFour
+                //    enemyAI.transform.LookAt(enemyAI.PatrolList[randFour].transform);
+                //    enemyAI.transform.position += enemyAI.transform.forward * speed;
+                //} 
+            //}
         }
         
         
