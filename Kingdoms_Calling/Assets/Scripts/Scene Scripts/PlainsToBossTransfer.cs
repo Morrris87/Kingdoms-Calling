598596b990b4f355c2d7b465aa6c40cc76f90f2e
cloochislegend <59626674+cloochislegend@users.noreply.github.com>
2020-04-02 19:36:21 -0400

    Added Level Loads for Scenes

---
+++ Kingdoms_Calling/Assets/Scripts/Scene Scripts/PlainsToBossTransfer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlainsToBossTransfer : MonoBehaviour
+{
+    public Image UIFade;
+
+    private float fadeLength = 2f;
+    private float fadeTimer;
+    private bool changeScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fadeTimer = fadeLength;
+        changeScenes = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeScenes)
+        {
+            fadeTimer -= Time.deltaTime;
+        }
+
+        if (fadeTimer <= 0f)
+        {
+            ChangeScene("BOSSFIGHT1");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            changeScenes = true;
+            FadeOutScreen();
+        }
+    }
+
+    public void FadeOutScreen()
+    {
+        UIFade.color = Color.black;
+        UIFade.canvasRenderer.SetAlpha(0.0f);
+        UIFade.CrossFadeAlpha(1.0f, fadeLength, false);
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
